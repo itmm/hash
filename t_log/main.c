0d7b22658f052e4e6727708d3201f46f958c508b
Timm Knape <timm.knape@dvag.com>
2014-03-14 08:55:00 +0100

    testing log type

--- t_log/main.c
+++ t_log/main.c
@@ -5,8 +5,11 @@
 #include <stdio.h>
 #include <string.h>
 
+#pragma mark - log mock
+
 static log_handler *_oldLogHandler;
 static char *_gotMessage;
+static const char *_gotType;
 
 const size_t max_log_size = 1024;
 
@@ -22,13 +25,20 @@ static void _testing_log_handler(const char *file, int line, const char *type, c
             log_error_with_handler(log_default_handler, "can't allocate log message");
         }
     } else {
-        log_error_with_handler(log_default_handler, "already logged %s", _gotMessage);
+        log_error_with_handler(log_default_handler, "already logged message %s", _gotMessage);
+    }
+    
+    if (!_gotType) {
+        _gotType = type;
+    } else {
+        log_error_with_handler(log_default_handler, "already logged type %s", _gotType);
     }
 }
 
 static void _setup_single_logger(unit_test *test) {
     _oldLogHandler = log_set_handler(_testing_log_handler);
     _gotMessage = NULL;
+    _gotType = NULL;
 }
 
 static void _teardown_single_logger(unit_test *test) {
@@ -39,15 +49,40 @@ static void _teardown_single_logger(unit_test *test) {
     }
 }
 
+
+#pragma mark - tests
+
 static void _log_empty_message(unit_test *test) {
     log_info("");
     assert_eq_str(test, "", _gotMessage);
 }
 
+static void _log_formatted_message(unit_test *test) {
+    log_error("n: %d, s: %s", 3, "abc");
+    assert_eq_str(test, "n: 3, s: abc", _gotMessage);
+}
+
+static void _log_info_message(unit_test *test) {
+    log_info("");
+    assert_eq_str(test, "info", _gotType);
+}
+
+static void _log_error_message(unit_test *test) {
+    log_error("");
+    assert_eq_str(test, "ERROR", _gotType);
+}
+
+
+#pragma mark - suite
+
 int main(int argc, const char * argv[])
 {
     unit_test *suite = test_suite_alloc("log tests",
-        test_full_alloc("log empty message", _setup_single_logger, _log_empty_message, _teardown_single_logger)
+        test_full_alloc("log empty message", _setup_single_logger, _log_empty_message, _teardown_single_logger),
+        test_full_alloc("log formatted message", _setup_single_logger, _log_formatted_message, _teardown_single_logger),
+        test_full_alloc("log info message", _setup_single_logger, _log_info_message, _teardown_single_logger),
+        test_full_alloc("log error message", _setup_single_logger, _log_error_message, _teardown_single_logger),
+        NULL
     );
     
     if (!suite) {
