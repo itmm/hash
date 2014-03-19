26316e40a5d779c4e4c36e87d6a72b486f2e6f72
Timm Knape <timm.knape@dvag.com>
2014-03-19 09:00:47 +0100

    knoten in t_rclist schöner erzeugen

--- tests/t_rclist.c
+++ tests/t_rclist.c
@@ -1,95 +1,77 @@
 #include "t_rclist.h"
 
+#include "log.h"
 #include "rc.h"
 #include "rclist.h"
 #include "rcstr.h"
 
 #pragma mark - tests
 
+static rclist *create_node(const char *key, const char *value, void *next) {
+    rcstr rckey = key ? rcstr_dup(key) : NULL;
+    rcstr rcvalue = value ? rcstr_dup(value) : NULL;
+    rclist *result = rclist_cons(rckey, rcvalue, next);
+    rc_release(rckey);
+    rc_release(rcvalue);
+    return_value_unless(result, NULL);
+    return result;
+}
+
 static void _empty_list(unit_state *state) {
-    rclist *lst = rclist_cons(NULL, NULL, NULL);
-    assert(state, lst, "must not be NULL");
+    rclist *lst = create_node(NULL, NULL, NULL);
+    return_unless(lst);
+
     assert_eq(state, NULL, lst->key);
     assert_eq(state, NULL, lst->value);
     assert_eq(state, NULL, lst->next);
+
     rc_release(lst);
+}
+
+static void _test_str(unit_state *state, rclist *lst, const char *expected) {
+    return_unless(lst);
+    
+    rcstr got = rclist2str(lst);
+    return_unless(got);
+    
+    rc_release(lst);
+    assert_eq_str(state, expected, got);
+    rc_release(got);
 }
 
 static void _empty_list_string(unit_state *state) {
-    rclist *lst = rclist_cons(NULL, NULL, NULL);
-    rcstr s = rclist2str(lst);
-    rc_release(lst);
-    assert_eq_str(state, ":", s);
-    rc_release(s);
+    _test_str(state, create_node(NULL, NULL, NULL), ":");
 }
 
 static void _simple_list(unit_state *state) {
-    rcstr key = rcstr_dup("key");
-    rcstr value = rcstr_dup("value");
-    rclist *lst = rclist_cons(key, value, NULL);
-    rc_release(key);
-    rc_release(value);
-    assert(state, lst, "must not be NULL");
+    rclist *lst = create_node("key", "value", NULL);
+    return_unless(lst);
+    
     assert_eq_str(state, "key", lst->key);
     assert_eq_str(state, "value", lst->value);
     assert_eq(state, NULL, lst->next);
+
     rc_release(lst);
 }
 
 static void _simple_list_string(unit_state *state) {
-    rcstr key = rcstr_dup("key");
-    rcstr value = rcstr_dup("value");
-    rclist *lst = rclist_cons(key, value, NULL);
-    rc_release(key);
-    rc_release(value);
-    rcstr s = rclist2str(lst);
-    rc_release(lst);
-    assert_eq_str(state, "key:value", s);
-    rc_release(s);
+    _test_str(state, create_node("key", "value", NULL), "key:value");
 }
 
 static void _strange_list_string(unit_state *state) {
-    rcstr key = rcstr_dup("key");
-    rcstr value = rcstr_dup("value");
     rcstr rest = rcstr_dup("rest");
-    rclist *lst = rclist_cons(key, value, rest);
-    rc_release(key);
-    rc_release(value);
+    _test_str(state, create_node("key", "value", rest), "key:value . rest");
     rc_release(rest);
-    rcstr s = rclist2str(lst);
-    rc_release(lst);
-    assert_eq_str(state, "key:value . rest", s);
-    rc_release(s);
 }
 
 static void _double_list_string(unit_state *state) {
-    rcstr key_1 = rcstr_dup("a");
-    rcstr value_1 = rcstr_dup("x");
-    rclist *tail = rclist_cons(key_1, value_1, NULL);
-    rc_release(key_1);
-    rc_release(value_1);
-    
-    rcstr key_2 = rcstr_dup("b");
-    rcstr value_2 = rcstr_dup("y");
-    rclist *head = rclist_cons(key_2, value_2, tail);
+    rclist *tail = create_node("a", "x", NULL);
+    _test_str(state, create_node("b", "y", tail), "b:y a:x");
     rc_release(tail);
-    
-    rcstr s = rclist2str(head);
-    rc_release(head);
-    assert_eq_str(state, "b:y a:x", s);
-    rc_release(s);
 }
 
 static void _colon_key(unit_state *state) {
-    rcstr key = rcstr_dup(":[");
-    rcstr value = rcstr_dup("value");
-    rclist *lst = rclist_cons(key, value, NULL);
-    rc_release(key);
-    rc_release(value);
-    rcstr s = rclist2str(lst);
-    rc_release(lst);
-    assert_eq_str(state, "[:\\[]:value", s);
-    rc_release(s);
+    _test_str(state, create_node(":[", "value", NULL), "[:\\[]:value");
 }
 
 #pragma mark - suite
